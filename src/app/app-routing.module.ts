67a5b5fce6e8e90dea162315bfc0617eeafeb0a9
kelvin muiruri <kelvinmuiruri51@gmail.com>
2022-11-23 12:11:32 +0300

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,33 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {ArtistsComponent} from './artists/artists.component';
+import {ArtistDetailComponent} from './artist-detail/artist-detail.component';
+import {AlbumComponent} from './album/album.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'artists',
+    component: ArtistsComponent
+  },
+  {
+    path: 'album',
+    component: AlbumComponent
+  },
+  {
+    path: 'details/:id',
+    component: ArtistDetailComponent
+  },
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'artist'
+  },
+  {
+    path: '**',
+    pathMatch: 'full',
+    component: ArtistsComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ArtistsComponent } from './artists/artists.component';
+import { ArtistComponent } from './artist/artist.component';
+import { ArtistDetailComponent } from './artist-detail/artist-detail.component';
+import { AlbumComponent } from './album/album.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ArtistsComponent,
+    ArtistComponent,
+    ArtistDetailComponent,
+    AlbumComponent
   ],
   imports: [
     BrowserModule,
